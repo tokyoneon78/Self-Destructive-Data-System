28be0c5873df522303b1baaa41e3865ba50fa35e
tokyoneon78 <75145552+tokyoneon78@users.noreply.github.com>
2022-05-17 16:45:09 +0100

    Creation of nodes

---
+++ Node.py
@@ -0,0 +1,28 @@
+
+import logging
+import asyncio
+
+from kademlia.network import Server
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log = logging.getLogger('kademlia')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+bootstrap_node = ("127.0.0.1", int("8468"))
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+
+server = Server()
+loop.run_until_complete(server.listen(8469))
+loop.run_until_complete(server.bootstrap([bootstrap_node]))
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    server.stop()
+    loop.close()
